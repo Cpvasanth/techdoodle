76b662ad7cb4fedbf5cae388c25f2d6958a6dc07
Cpvasanth <cpvasanthakumar@gmail.com>
2025-03-26 21:06:48 +0530

    Update HeroSection to link to new Tech Doodle 2K25 PDF and remove outdated rules document

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Countdown from "./Countdown";
 import "../styles/Hero.css";
 import srecLogo from "../assets/images/logo.png";
-import pdf from "../assets/TECHDOODLE_2K25_RULES_AND_GUIDELINES.pdf";
+import pdf from "../assets/TECHDOODLE_2K25.pdf";
 
 export default function HeroSection() {
   const techDoodleTarget = new Date(new Date().getFullYear(), 2, 28).getTime(); // March 28
