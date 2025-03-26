5ea3559698ba960c577d1a188a245a2cd87a752e
Cpvasanth <cpvasanthakumar@gmail.com>
2025-03-26 21:02:07 +0530

    Update event details with venues for improved clarity and information display

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -21,19 +21,22 @@ export default function Event() {
         <div className="event-detail">
           <FontAwesomeIcon icon={faChalkboardTeacher} />
           <h3>Paper presentation <br></br></h3>
-          {/* <p>Showcase innovative ideas through research and presentations in symposium. <br/> <span style={{color: "red"}}>Paper submission last date: 15TH March 2025</span></p> */}
+          <p>Showcase innovative ideas through research and presentations in symposium</p>
+          <p style={{ color: "black" }}>venue: AI & DS Lab</p>
         </div>
 
         <div className="event-detail">
           <FontAwesomeIcon icon={faCode} />
           <h3>PIXEL DOODLE</h3>
           <p>Analyze, identify, and solve coding issues in real-time challenges.</p>
+          <p style={{ color: "black" }}>Venue: CSBS Lab</p>
         </div>
 
         <div className="event-detail">
           <FontAwesomeIcon icon={faBrain} />
           <h3>CIPHER DOODLE <br/> </h3>
           <p>Test your knowledge with exciting cryptography challenges.</p>
+          <p style={{ color: "black" }}>Venue: CSBS Lab</p>
         </div>
         </div>
 
@@ -45,18 +48,23 @@ export default function Event() {
           <FontAwesomeIcon icon={faTrophy} />
           <h3>TRESURE HUNT</h3>
           <p>Treasure Hunt! Solve clues, explore, and race to uncover hidden treasures. Adventure and fun await!</p>
+          <p style={{ color: "black" }}>Venue: Mini auditorium</p>
+
         </div>
 
         <div className="event-detail">
           <FontAwesomeIcon icon={  faCircleNodes} />
           <h3>CONNECTION</h3>
           <p>Decode the mystery! Connect the given images to reveal a Song, word, or movie Think fast, link smart, and crack the puzzle! 🧩🎬🔥</p>
+          <p style={{ color: "black" }}>Venue: Placement cell</p>
         </div>
 
         <div className="event-detail">
           <FontAwesomeIcon icon={faPersonRifle} />
           <h3>FREE FIRE</h3>
           <p>🔥 Free Fire Esports Tournament! 🔥 Compete, strategize, and conquer the battlefield. Show your skills and claim victory! 🎮🏆</p>
+          <p style={{ color: "black" }}>Venue: Room no.308</p>
+
         </div>
         </div>
 
